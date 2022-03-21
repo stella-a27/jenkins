bc0cbda28c15063da593e14d714fc6b6fb1616b1
Stellaantony.P <Stellaantony.P@EXIND3010-142.allfon.in>
2022-03-21 11:02:05 +0530

    project completed

---
+++ jenkins-git-maven-docker-build/src/main/java/com/example/demo/controllers/TeacherController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controllers;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1")
+public class TeacherController {
+
+	@GetMapping(path ="/teachers")
+	public String getTeacher() {
+		
+		List<String> list = Arrays.asList("Stella","Kevin","Sree");
+		return list.toString();
+		
+	}
+}
